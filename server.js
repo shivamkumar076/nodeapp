aa74353f148e5749386dff86ba74a1f203af62a7
shivam kumar <shivamkumar776499@gmail.com>
2026-07-14 10:02:41 +0530

    dockerfile

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // Server Start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
